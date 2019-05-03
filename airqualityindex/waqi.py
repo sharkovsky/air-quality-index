d25a16326a2bc6e3acd832116714e2d32ef8fdf6
sharkovsky <francesco.cremonesi@epfl.ch>
2019-05-03 14:49:39 +0200

    Adding docstrings for main module

--- airqualityindex/waqi.py
+++ airqualityindex/waqi.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+"""World Air Quality Index (WAQI).
+
+This module implements the necessary logic to retrieve the Air Quality Index of a
+given city, by contacting the WAQI REST API.
+It also tries to handle 404 errors.
+
+Example:
+    To obtain information for Hong Kong:
+
+    $ python -m airqualityindex.waqi --city hongkong
+"""
+
+
 import requests
 
 
@@ -5,7 +19,21 @@ __all__ = ['WAQI']
 
 
 class WAQI:
+    """Air Quality Index from waqi REST API.
+
+    This class can obtain the Air Quality Index by making
+    requests to the waqi API.
+
+    Attributes:
+        token: a string containing the API token
+    """
+
     def __init__(self, token_):
+        """Inits WAQI with an API token.
+
+        Args:
+            token_: a string containing the API token
+        """
         self.token = token_
         self._session = requests.Session()
         self._session.params = {'token': self.token}
@@ -15,15 +43,37 @@ class WAQI:
         return 'http://api.waqi.info'
 
     def _request_city_feed(self, city_name):
+        """Performs the API request."""
         request_url = self._domain_url + '/feed/' + city_name + '/'
         res = self._session.get(request_url)
         return res
 
     def _behaviour_in_case_of_404_error(self, response):
-        # do other things before
+        """Implements requested behaviour in case of 404.
+
+        This is a dummy function to help showcase
+        mocking in tests.
+        """
         response.raise_for_status()
 
     def get_air_quality(self, city_name):
+        """Get air quality index from API.
+
+        Peforms a request to the API, tries to handle 404 errors
+        and packages the results in a dictionary.
+
+        Args:
+            city_name: a string with the name of the city to be
+                queried. This name *must* be in the format
+                that the waqi API accepts.
+
+        Returns:
+            A dict with keys
+                city: a string with a nicely formatted name of
+                    the queried city, as returned by the waqi API.
+                aqi: a float value of the air quality index.
+        """
+
         res = self._request_city_feed(city_name)
 
         if res.status_code == requests.codes.not_found:
